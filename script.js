417da921ebfa23d12c9edc245aa931d9d6fef544
Nayan Chauhan <nayanchauhan9999@gmail.com>
2023-01-21 12:39:38 +0530

    committed

---
+++ script.js
@@ -0,0 +1,8 @@
+let search = document.getElementById("search");
+let button = document.getElementById("btn");
+let searchPopup = document.getElementById("search-popup")
+
+search.addEventListener("click",()=>{
+    button.classList.toggle("hiddenbtn");
+    searchPopup.classList.toggle("hiddenbtn");
+})
