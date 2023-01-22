8646d78b7945012c67f2e6250a4f2f064ed2fcaf
Nayan Chauhan <nayanchauhan9999@gmail.com>
2023-01-22 21:25:04 +0530

    i commit

--- script.js
+++ script.js
@@ -7,3 +7,12 @@ search.addEventListener("click",()=>{
     searchPopup.classList.toggle("hiddenbtn");
     search.classList.toggle("searchbackgroundcolor")
 })
+
+let toggleLeft = document.getElementsByClassName("toggleclass")[0];
+let hamburger = document.getElementById("hamburger")
+hamburger.addEventListener("click",()=>{
+    toggleLeft.style.left="0"
+})
+toggleLeft.addEventListener("mouseleave",()=>{
+    toggleLeft.style.left="-230px"
+})
