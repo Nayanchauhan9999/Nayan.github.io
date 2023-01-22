a794f73153d98ecb34cb8032455fc9429b68d6d1
Nayan Chauhan <nayanchauhan9999@gmail.com>
2023-01-22 21:40:17 +0530

    1

--- script.js
+++ script.js
@@ -8,11 +8,11 @@ search.addEventListener("click",()=>{
     search.classList.toggle("searchbackgroundcolor")
 })
 
-let toggleLeft = document.getElementsByClassName("toggleclass")[0];
-let hamburger = document.getElementById("hamburger")
-hamburger.addEventListener("click",()=>{
-    toggleLeft.style.left="0"
-})
-toggleLeft.addEventListener("mouseleave",()=>{
-    toggleLeft.style.left="-230px"
-})
+// let toggleLeft = document.getElementsByClassName("toggleclass")[0];
+// let hamburger = document.getElementById("hamburger")
+// hamburger.addEventListener("click",()=>{
+//     toggleLeft.style.left="0"
+// })
+// toggleLeft.addEventListener("mouseleave",()=>{
+//     toggleLeft.style.left="-230px"
+// })
